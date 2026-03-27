2e79a35fbeabd2230f184c7a0f66a06bb2f45b8a
jiayitang0511-wq <jiayi.tang0511@gmail.com>
2026-03-26 21:56:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello Jiayi!');
+    res.send('Hello World!');
 });
 
 app.get('/api/health', (req, res) => {
